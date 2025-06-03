eb6ccc81a9ba7cb318f706d10ed41f13abc5aa6c
Laurent Pellegrino <laurent.pellegrino@gmail.com>
2025-06-03 14:52:12 +0200

    Fix export

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true // ← tell <Image> to render a plain <img>
+  },
+  output: 'export',
   reactStrictMode: true
 };
 export default nextConfig;
